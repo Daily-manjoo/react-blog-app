f14c095fd08cbcb550f2ecff2a29152acaadf548
Daily-manjoo <yuslz35@gmail.com>
2023-11-24 00:35:24 +0900

    캐러셀, 폼 레이아웃 구현

--- src/components/PostList.tsx
+++ src/components/PostList.tsx
@@ -1,16 +1,24 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 interface PostListProps {
     hasNavigation?: boolean;
 }
 
+type TabType = "all" | "my";
+
 export default function PostList({ hasNavigation = true}: PostListProps){ //profile페이지에선 안보이게
+    const [activeTab, setActiveTab] = useState<TabType>("all");
     return (
         <>
         {hasNavigation && (
             <div className="post__navigation">
-            <div className="post__navigation--active">전체글</div>
-                <div>나의 글</div>
+                <div role="presentation" 
+                onClick={() => setActiveTab("all")}
+                className={activeTab === 'all' ? 'post__navigation--active' : ""}>전체글</div>
+                <div role="presentation" 
+                onClick={() => setActiveTab("my")}
+                className={activeTab === 'my' ? 'post__navigation--active' : ""}>나의 글</div>
             </div>
         )}
         <div className="post__list">
--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -1,12 +1,12 @@
 import { Route, Routes, Navigate} from 'react-router-dom'; // Route:경로매칭, Routes: 라우트 감싸고있음, Navigate: 정의하지않은 경로 진입시 메인으로
-import Home from '../pages/home';
-import PostList from '../pages/posts';
-import PostDetail from '../pages/posts/detail';
-import PostNew from '../pages/posts/new';
-import PostEdit from '../pages/posts/edit';
-import ProfilePage from '../pages/profile';
-import LoginPage from '../pages/login';
-import SignupPage from '../pages/signup';
+import Home from 'pages/home';
+import PostList from 'pages/posts';
+import PostDetail from 'pages/posts/detail';
+import PostNew from 'pages/posts/new';
+import PostEdit from 'pages/posts/edit';
+import ProfilePage from 'pages/profile';
+import LoginPage from 'pages/login';
+import SignupPage from 'pages/signup';
 
 
 export default function Router() {
