e21745ded7ea5025289f4f029ed83b59da5e1903
Daily-manjoo <yuslz35@gmail.com>
2023-11-29 23:21:06 +0900

    게시글 읽기 구현

--- src/components/PostDetail.tsx
+++ src/components/PostDetail.tsx
@@ -1,30 +1,58 @@
-import { Link } from "react-router-dom"
+import { Link, useParams } from "react-router-dom"
+import { useState, useEffect } from "react";
+import { PostProps } from "./PostList";
+import { doc, getDoc } from "firebase/firestore";
+import { db } from "firebaseApp";
+import Loader from "./Loader";
 
 export default function PostDetail(){
+    const [post, setPost] = useState<PostProps | null>(null);
+    const params = useParams(); //id 값 가져오기위해
+
+    const getPost = async(id: string) => {
+            if(id){
+                const docRef = doc(db, 'posts', id);
+                const docSnap = await getDoc(docRef);
+                // console.log(docSnap?.data());
+
+                // ID값도 같이 가져와야해서
+                setPost({id: docSnap.id, ...docSnap.data() as PostProps})
+            }
+    }
+
+    const handleDelete = () => {
+        console.log("Delete!")
+    }
+
+    useEffect(() => {
+        if(params?.id) getPost(params?.id);
+    }, [params?.id])
+
     return(
         <>
         <div className="post__detail">
+            {post ? (
             <div className="post__box">
-                <div className="post__title">유리, 곱창 너무 많이 먹어..</div>
+                <div className="post__title">{post?.title}.</div>
                 <div className="post__profile-box">
                             <div className="post__profile" />
-                            <div className="post__author-name">유리</div>
-                            <div className="post__date">2023.11.20 월요일</div>
+                            <div className="post__author-name">{post?.email}</div>
+                            <div className="post__date">{post?.createAt}</div>
                         </div>
-                        <div className="post__title">게시글</div>
+                        <div className="post__title">{post?.title}</div>
                         <div className="post__utils-box">
-                            <div className="post__delete">삭제</div>
+                            <div className="post__delete" role="presentation" onClick={handleDelete}>삭제</div>
                             <div className="post__edit">
                                 <Link to={`/posts/edit/1`} >수정</Link>
                             </div>
                         </div>
-                        <div className="post__text">
-                            유리와 진수<br/>
-                            강아지과 고양이<br/>
-                            초밥 대 곱창<br />
-                            창과 방패<br />
+                        <div className="post__tex post__text--pre-wrap">
+                            {post?.content}<br/>
                         </div>
                 </div>
+                ) : (
+                <Loader />
+                 )}
             </div>
         </>
     )
--- src/components/PostList.tsx
+++ src/components/PostList.tsx
@@ -1,5 +1,8 @@
 import { Link } from "react-router-dom";
-import { useState } from "react";
+import { useContext, useEffect, useState } from "react";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "firebaseApp";
+import AuthContext from "context/AuthContext";
 
 interface PostListProps {
     hasNavigation?: boolean;
@@ -7,8 +10,35 @@ interface PostListProps {
 
 type TabType = "all" | "my";
 
+export interface PostProps {
+    id?: string;
+    title: string;
+    email: string;
+    summary: string;
+    content: string;
+    createAt: string;
+}
+
 export default function PostList({ hasNavigation = true}: PostListProps){ //profile페이지에선 안보이게
     const [activeTab, setActiveTab] = useState<TabType>("all");
+    const [posts, setPosts] = useState<PostProps[]>([]);
+    const { user } = useContext(AuthContext);
+
+    const getPosts = async() => {
+        const datas = await getDocs(collection(db, "posts"));
+
+        datas?.forEach((doc) => {
+            console.log(doc.data(), doc.id);
+            const dataObj = { ...doc.data(), id: doc.id };
+            setPosts((prev) => [...prev, dataObj as PostProps]);
+        })
+    }
+
+
+    useEffect(()=> {
+        getPosts();
+    }, [])
+
     return (
         <>
         {hasNavigation && (
@@ -22,28 +52,28 @@ export default function PostList({ hasNavigation = true}: PostListProps){ //prof
             </div>
         )}
         <div className="post__list">
-                {[...Array(10)].map((e, index)=> (
-                    <div key={index} className="post__box">
-                    <Link to={`/posts/${index}`}>
+                {posts?.length>0 ? posts?.map((post, index)=> (
+                    <div key={post?.id} className="post__box">
+                    <Link to={`/posts/${post?.id}`}>
                         <div className="post__profile-box">
                             <div className="post__profile" />
-                            <div className="post__author-name">유리</div>
-                            <div className="post__date">2023.11.20 월요일</div>
+                            <div className="post__author-name">{post?.email}</div>
+                            <div className="post__date">{post?.createAt}</div>
                         </div>
-                        <div className="post__title">게시글 {index}</div>
-                        <div className="post__text">
-                            유리와 진수<br/>
-                            강아지과 고양이<br/>
-                            초밥 대 곱창<br />
-                            창과 방패<br />
-                        </div>
-                        <div className="post__utils-box">
+                        <div className="post__title">{post?.title}</div>
+                        <div className="post__text">{post?.summary}</div>
+                        </Link>
+                        
+                        {post?.email === user?.email && (
+                            <div className="post__utils-box">
                             <div className="post__delete">삭제</div>
-                            <div className="post__edit">수정</div>
+                            <div className="post__edit">
+                            <Link to={`/posts/edit/${post?.id}`} >수정</Link>
+                            </div>
                         </div>
-                    </Link>
+                        )} 
                     </div>
-                ))}
+                )): <div className="post__no-post">게시글이 없습니다.</div>}
             </div>
             </>
     )
